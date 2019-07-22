d2a0c1b4c3a3bebb06f5ba9c0c216f1df53a5064
shulinqian <330576744@qq.com>
2019-07-22 17:05:47 +0800

    修复粘包问题

--- src/config/tcp.php
+++ src/config/tcp.php
@@ -12,6 +12,11 @@ return [
     'swoole' => [
         'worker_num' => 5,
         'max_request' => 1000000,
+        'package_length_type' => 'N',
+        'package_length_offset' => 0,
+        'package_body_offset' => 4,
+        'package_max_length' => 1024 * 1024 * 3, //包大小为3M
+        'buffer_output_size' => 1024 * 1024 * 3,//输出缓存区大小
         'log_file' => SUMMER_APP_ROOT . 'runtime/swoole.log',
         'pid_file' => SUMMER_APP_ROOT . 'runtime/swoole.pid'
     ],
